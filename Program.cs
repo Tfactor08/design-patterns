cb4f4c44d5f69f67caf72d0e6b2eedd3024c42d6
Tfactor2020@gmail.com <Tfactor08>
2025-02-26 22:47:09 +0300

    delete redundant files

--- Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using PatternsConsoleApp;
-
-string sourceCode = "quick brown fox jumps over the lazy dog";
-
-Compiler compiler = new Compiler(sourceCode);
-var compilerWithPreprocessor = new Preprocessor(compiler);
-var result = compilerWithPreprocessor.Compile();
-
-Console.WriteLine(result);
