3d6c13085e18107fb52ed960fbf56586db9f6b17
Student <Student@NCH-02-406-14>
2025-03-05 14:44:09 +0300

    add todos

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,11 @@
 ﻿using PatternsConsoleApp;
 
+// todo:
+// format compiler output properly;
+// implement arithmetic expressions evaluation as a compilaton step;
+// implement macros substitutions as a preoprocessing step;
+// rename current branch and create new one
+
 string sourceCode = "quick brown fox jumps over the lazy dog";
 
 Compiler compiler = new Compiler(sourceCode);
