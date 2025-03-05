4d5df8bf94664980354dd2bfaa2d21f842c61cdb
Student <Student@NCH-02-406-14>
2025-03-05 16:44:25 +0300

    add extesnsion method for correct indenting

---
+++ src/utils/Extensions.cs
@@ -0,0 +1,13 @@
+﻿namespace PatternsConsoleApp.src.utils;
+
+public static class Extensions
+{
+    public static string IndentLines(this string str, string indent)
+    {
+        string[] lines = str.Split('\n');
+        for (int i = 0; i < lines.Length; i++)
+            lines[i] = indent + lines[i];
+
+        return string.Join('\n', lines);
+    }
+}
